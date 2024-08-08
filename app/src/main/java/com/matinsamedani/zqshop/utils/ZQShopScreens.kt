45b97e1e4d49048d420ba710a4ba38fd1f1415e4
Shahram <shahramdeveloper11@gmail.com>
2024-08-08 18:10:52 +0330

    ZQShopScreens.kt created in project and all screens added to ZQShopScreens.kt

---
+++ app/src/main/java/com/matinsamedani/zqshop/utils/ZQShopScreens.kt
@@ -0,0 +1,18 @@
+package com.matinsamedani.zqshop.utils
+
+sealed class ZQShopScreens(val route: String) {
+    //Welcome screen
+    data object Welcome : ZQShopScreens("Welcome")
+
+    //Login screens
+    data object SignUpScreen : ZQShopScreens("SignUpScreen")
+    data object SignInScreen : ZQShopScreens("SignInScreen")
+
+    //Main screens
+    data object HomeScreen : ZQShopScreens("HomeScreen")
+    data object ExploreScreen : ZQShopScreens("ExploreScreen")
+    data object ProfileScreen : ZQShopScreens("ProfileScreen")
+    data object CartScreen : ZQShopScreens("CartScreen")
+    data object CategoryScreen : ZQShopScreens("CategoryScreen")
+    data object ProductScreen : ZQShopScreens("ProductScreen")
+}
