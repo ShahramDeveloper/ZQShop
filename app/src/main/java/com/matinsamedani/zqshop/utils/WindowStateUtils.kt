895c1bd63785a9c440af43b6d345085a5e4d1e71
Shahram <shahramdeveloper11@gmail.com>
2024-08-10 15:23:03 +0330

    WindowStateUtils.kt created in project. DevicePosture for Foldable device added to WindowStateUtils.kt. isBookPosture and isSeparating added to it. ZQShopNavigationType for navigation type in project added to it. ZQShopContentType added to it.

---
+++ app/src/main/java/com/matinsamedani/zqshop/utils/WindowStateUtils.kt
@@ -0,0 +1,44 @@
+package com.matinsamedani.zqshop.utils
+
+import android.graphics.Rect
+import androidx.window.layout.FoldingFeature
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
+
+sealed interface DevicePosture {
+    data object NormalPosture : DevicePosture
+
+    data class BookPosture(
+        val hingePosition: Rect
+    ) : DevicePosture
+
+    data class Separating(
+        val hingePosition: Rect,
+        var orientation: FoldingFeature.Orientation
+    ) : DevicePosture
+}
+
+@OptIn(ExperimentalContracts::class)
+fun isBookPosture(foldFeature: FoldingFeature?): Boolean {
+    contract { returns(true) implies (foldFeature != null) }
+    return foldFeature?.state == FoldingFeature.State.HALF_OPENED &&
+            foldFeature.orientation == FoldingFeature.Orientation.VERTICAL
+}
+
+@OptIn(ExperimentalContracts::class)
+fun isSeparating(foldFeature: FoldingFeature?): Boolean {
+    contract { returns(true) implies (foldFeature != null) }
+    return foldFeature?.state == FoldingFeature.State.FLAT && foldFeature.isSeparating
+}
+
+enum class ZQShopNavigationType {
+    BOTTOM_NAVIGATION, NAVIGATION_RAIL, PERMANENT_NAVIGATION_DRAWER
+}
+
+enum class ZQShopNavigationContentPosition {
+    TOP, CENTER
+}
+
+enum class ZQShopContentType {
+    SINGLE_PANE, DUAL_PANE
+}
