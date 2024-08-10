4a035f6fa7e559d2384692d241ea942ff9e79d42
Shahram <shahramdeveloper11@gmail.com>
2024-08-10 16:12:20 +0330

    Preview function in all size device added to MainActivity.kt. ZQShopApp called in MainActivity.kt.

--- app/src/main/java/com/matinsamedani/zqshop/MainActivity.kt
+++ app/src/main/java/com/matinsamedani/zqshop/MainActivity.kt
@@ -8,15 +8,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.adaptive.calculateDisplayFeatures
 import com.matinsamedani.zqshop.di.ZQShopModules
 import com.matinsamedani.zqshop.ui.theme.ZQShopTheme
 import dev.burnoo.cokoin.Koin
 import org.koin.android.ext.koin.androidContext
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -28,30 +35,75 @@ class MainActivity : ComponentActivity() {
                 }
             ) {
                 ZQShopTheme {
-                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                        Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
-                        )
-                    }
+                    val windowSize = calculateWindowSizeClass(this)
+                    val displayFeatures = calculateDisplayFeatures(this)
+
+                    ZQShopApp(
+                        windowSize = windowSize,
+                        displayFeatures = displayFeatures
+                    )
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+@Preview(showBackground = true, widthDp = 400, heightDp = 900)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ReplyAppPreview() {
+    ZQShopTheme {
+        ZQShopApp(
+            windowSize = WindowSizeClass.calculateFromSize(DpSize(400.dp, 900.dp)),
+            displayFeatures = emptyList(),
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+@Preview(showBackground = true, widthDp = 700, heightDp = 500)
 @Composable
-fun GreetingPreview() {
+fun ReplyAppPreviewTablet() {
     ZQShopTheme {
-        Greeting("Android")
+        ZQShopApp(
+            windowSize = WindowSizeClass.calculateFromSize(DpSize(700.dp, 500.dp)),
+            displayFeatures = emptyList(),
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+@Preview(showBackground = true, widthDp = 500, heightDp = 700)
+@Composable
+fun ReplyAppPreviewTabletPortrait() {
+    ZQShopTheme {
+        ZQShopApp(
+            windowSize = WindowSizeClass.calculateFromSize(DpSize(500.dp, 700.dp)),
+            displayFeatures = emptyList(),
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+@Preview(showBackground = true, widthDp = 1100, heightDp = 600)
+@Composable
+fun ReplyAppPreviewDesktop() {
+    ZQShopTheme {
+        ZQShopApp(
+            windowSize = WindowSizeClass.calculateFromSize(DpSize(1100.dp, 600.dp)),
+            displayFeatures = emptyList(),
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+@Preview(showBackground = true, widthDp = 600, heightDp = 1100)
+@Composable
+fun ReplyAppPreviewDesktopPortrait() {
+    ZQShopTheme {
+        ZQShopApp(
+            windowSize = WindowSizeClass.calculateFromSize(DpSize(600.dp, 1100.dp)),
+            displayFeatures = emptyList(),
+        )
     }
 }
