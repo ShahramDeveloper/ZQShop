127fbb658964d17a2b57fb4555ba2d9dc44db975
Shahram <shahramdeveloper11@gmail.com>
2024-08-17 00:59:30 +0330

    createApiService function added to Modules.kt.

--- app/src/main/java/com/shahram/zqshop/di/Modules.kt
+++ app/src/main/java/com/shahram/zqshop/di/Modules.kt
@@ -1,10 +1,12 @@
 package com.shahram.zqshop.di
 
 import android.content.Context
+import com.shahram.zqshop.data.network.createApiService
 import com.shahram.zqshop.utils.USER_DATA
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val ZQShopModules = module {
     single { androidContext().getSharedPreferences(USER_DATA, Context.MODE_PRIVATE) }
+    single { createApiService() }
 }
